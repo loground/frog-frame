1f49c144e28aa8f79c794e509af257ced84182a4
Никита Воронин <nickvoroninwork@gmail.com>
2024-05-19 19:00:43 +0800

    test of first frame

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,79 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog';
+import { devtools } from 'frog/dev';
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next';
+import { serveStatic } from 'frog/serve-static';
+import { pinata } from 'frog/hubs';
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+
+  // hub: pinata(),
+});
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  return c.res({
+    image: '/Battle.png',
+    action: '/utility',
+    title: 'Battle',
+    intents: [
+      <Button value="Battle Reply">Shit yes, I did</Button>,
+      <Button value="Battle Reply">Hell naw, I didnt</Button>,
+    ],
+  });
+});
+
+app.frame('/utility', (c) => {
+  return c.res({
+    image: '/Utility.png',
+    action: '/utility',
+    title: 'utility',
+    intents: [
+      <Button action="/bleu" value="Battle Reply">
+        HELL YEA 😤
+      </Button>,
+      <Button action="/bleu">NO 😭</Button>,
+    ],
+  });
+});
+
+app.frame('/bleu', (c) => {
+  console.log(c);
+  return c.res({
+    image: '/Bleu.png',
+    action: '/bleu',
+    title: 'Bleu',
+
+    intents: [
+      <Button.Link href="https://zora.co/collect/base:0x338da94719499060d4fe18121114fd24e8668220/1?referrer=0x2De78F2dB332726Bf6e45722A20E96118d3d2462">
+        Touch it
+      </Button.Link>,
+      <Button action="/to">Been there, what else?</Button>,
+    ],
+  });
+});
+
+app.frame('/to', (c) => {
+  console.log(c);
+  return c.res({
+    image: '/Trade.png',
+    title: 'trade',
+
+    intents: [
+      <Button.Link href="https://zora.co/collect/base:0x338da94719499060d4fe18121114fd24e8668220/2?referrer=0x2De78F2dB332726Bf6e45722A20E96118d3d2462">
+        HOLY WOW MINTING RN, thanks BOSS
+      </Button.Link>,
+    ],
+  });
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import { getFrameMetadata } from 'frog/next';
+import type { Metadata } from 'next';
+
+import styles from './page.module.css';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const frameTags = await getFrameMetadata(
+    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
+  );
+  return {
+    other: frameTags,
+  };
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+          <p>
+            Head to{' '}
+            <a href="/api/dev" style={{ display: 'inline', fontWeight: 'semibold' }}>
+              <code className={styles.code}>localhost:3000/api</code>
+            </a>{' '}
+            for your frame endpoint.
+          </p>
+          <p>You shouldn't miss those</p>
+          <a href="https://zora.co/collect/base:0x338da94719499060d4fe18121114fd24e8668220/1">
+            Elefant
+          </a>
+          <a href="https://zora.co/collect/base:0x338da94719499060d4fe18121114fd24e8668220/2">
+            Trade
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
